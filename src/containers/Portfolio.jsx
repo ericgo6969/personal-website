b31b9f08a14f7ceb916f0ad84759759dd6f498f1
thiennbt <thiennbt.work@gmail.com>
2022-05-05 20:25:02 +0700

    upload 5/5

--- src/containers/Portfolio.jsx
+++ src/containers/Portfolio.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Paper } from '@mui/material';
+import { Masonry } from '@mui/lab';
+// import Masonry from 'react-masonry-component';
 import "../assets/styles/components/portfolio/portfolio.css";
+import { display, height } from '@mui/system';
 
 // image
-import arrow from "../assets/images/arrow.svg";
+// import arrow from "../assets/images/arrow.svg";
+
+const heights = [27, 60, 40, 50, 40, 30, 48, 38, 30, 27];
 
 function Portfolio() {
     return (
@@ -11,11 +17,32 @@ function Portfolio() {
                 <div className="main">
                     <div className="left">
                         <div className="image">
-                            <img className='arrow-up' src={arrow} alt="arrow up icon" />
-                            <img src={arrow} alt="arrow down icon" />
+                            {/* <img className='arrow-up' src={arrow} alt="arrow up icon" />
+                            <img src={arrow} alt="arrow down icon" /> */}
                         </div>
                     </div>
-                    <div className="center"></div>
+                    <div className="center">
+                        <Masonry
+                            columns={{ xs: 1, sm: 2, md: 4 }}
+                            spacing={1}
+                        >
+                            {
+                                heights.map((height, index) => (
+                                    <Paper
+                                        key={index}
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            height: `${height}%`,
+                                            border: '1px solid'
+                                        }}>
+                                        {index + 1}
+                                    </Paper>
+                                ))
+                            }
+                        </Masonry>
+                    </div>
                     <div className="right"></div>
                 </div>
             </div>
